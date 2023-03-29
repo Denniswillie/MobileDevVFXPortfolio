c6a0f0ab7a3d3c26cea8a59f07772dd0a449f9e6
Dennis Willie <denniswillie2000@gmail.com>
2023-03-29 03:34:24 +0100

    Initial Commit

---
+++ MobileDevVFXPortfolio/MobileDevVFXPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileDevVFXPortfolioApp.swift
+//  MobileDevVFXPortfolio
+//
+//  Created by Dennis Willie on 29/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MobileDevVFXPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
